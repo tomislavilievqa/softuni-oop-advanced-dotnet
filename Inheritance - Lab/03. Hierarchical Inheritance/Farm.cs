037443ad0a5cf00a7bbf11d93ce71044024c883b
Tomislav Iliev <127593498+tomislavilievqa@users.noreply.github.com>
2023-09-05 11:00:14 +0300

    Create Inheritance - Exercise

---
+++ Inheritance - Lab/03. Hierarchical Inheritance/Farm.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Hierarchical_Inheritance
+{
+    public class Farm
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+
+        }
+    }
+}
