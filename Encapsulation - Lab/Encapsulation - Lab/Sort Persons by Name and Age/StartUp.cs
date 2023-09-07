bd3564f4bb4643f2aa8851f3c9e00ea3b5cfe6d9
Tomislav Iliev <127593498+tomislavilievqa@users.noreply.github.com>
2023-09-07 12:31:37 +0300

    update

---
+++ Encapsulation - Lab/Encapsulation - Lab/Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Sort_Persons_by_Name_and_Age;
+
+namespace PersonsInfo
+{
+    internal class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var peopleDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var person = new Person(peopleDetails[0], peopleDetails[1], int.Parse(peopleDetails[2].Trim()), 100);//decimal.Parse(peopleDetails[3].Trim()));
+                persons.Add(person);
+            }
+            var percentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p  => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            persons.OrderBy(p => p.FirstName).ThenBy(p => p.LastName).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+        }
+
+    }
+
+}
