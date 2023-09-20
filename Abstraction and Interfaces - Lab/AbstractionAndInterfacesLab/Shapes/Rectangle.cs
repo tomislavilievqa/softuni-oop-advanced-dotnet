2f2be20b01ea9400dd665d2cd490a8369a0261c3
Tomislav Iliev <127593498+tomislavilievqa@users.noreply.github.com>
2023-09-20 14:09:53 +0300

    adding a new solution

---
+++ Abstraction and Interfaces - Lab/AbstractionAndInterfacesLab/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class Rectangle : IDrawable
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', Width));
+            for (int row = 0; row < Height - 2; row++)
+            {
+                Console.WriteLine("*" + new string(' ', this.Width - 2) + "*");
+
+            }
+            Console.WriteLine(new string('*', Width));
+        }
+
+
+    }
+}
---
+++ Abstraction and Interfaces - Lab/AbstractionAndInterfacesLab/Shapes/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+
+
+        }
+    }
+}
